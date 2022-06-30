69d44339865f9955a2c61c6a8fef5ffc095c74ab
Foday Jihared Conteh <jiharedc@gmail.com>
2022-06-30 08:03:30 -0700

    using puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
+/**description of the main function**/
 int main(void)
 {
-// A c program that print using the puts fubction
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
