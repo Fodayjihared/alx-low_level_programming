e261029b6eaa27d976d1d9d7707122464dc53b85
Foday Jihared Conteh <jiharedc@gmail.com>
2022-07-06 11:32:28 -0700

    fixed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry point
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * print_alphabet - prints the lowercase alphabets
+ *
+ *
+ *
+ */
+#include <main.h>
+
+void print_alphabet(void)
+{
+int j;
+for (j = 'a' j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
