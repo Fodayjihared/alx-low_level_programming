4523dc79599e004b055420f49690f192017e5e09
Foday Jihared Conteh <jiharedc@gmail.com>
2022-07-18 12:40:33 -0700

    fixed description

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - This is memory set function
- * @s string
- * @b - a character
- * @n - an integer
- * Return: a string
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int a;
-    for (a = 0; a < n; a++)
-        s[a] = b;
+unsigned int a;
+for (a = 0; a < n; a++)
+s[a] = b;
 
-    return (s)
+return (s)
 }
