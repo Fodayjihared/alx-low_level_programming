fbc3e7bf3647708b063f0e293402bc4a6e8e65a5
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-11 15:09:38 -0700

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		if (alp == 'q' && alp == 'e')
+		{
+			continue;
+		} else
+		{
+			putchar(alp);
+		}
+		alp++;
+	}
+	return (0);
+}
