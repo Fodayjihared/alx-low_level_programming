a46f6af591df2959f5db1967caa4ac1710a13a40
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-11 14:41:06 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char current_char = 'a';
+
+	while (current_char <= 'z')
+	{
+		putchar(current_char);
+		current_char++;
+	}
+	putchar('\n');
+	return (0);
+}
