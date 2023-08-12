1d68c47a835a33c5f5344e35fca08faab7f135d4
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-12 01:04:22 -0700

    Reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char initial = 'z';
+
+	while (initial <= 'a')
+	{
+		putchar(initial);
+		--initial;
+	}
+	putchar('\n');
+	return (0);
+}
