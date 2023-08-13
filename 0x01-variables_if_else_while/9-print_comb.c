5417b666c74e19bb4b0cd8cb120af9fff424d289
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-12 17:37:56 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,18 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; ++i)
+	for (i = 0; i < 10; ++i)
+
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i <= 10)
+		{
+			putchar(',');
+			putchar(' ');
+			putchar(i + '0');
+		} else
+		{
+			break;
+		}
 	}
 	putchar('\n');
 	return (0);
