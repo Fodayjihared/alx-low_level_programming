210e2106e8263129274a676ce56be5dd77416c10
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-15 06:35:47 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prnts _putchar
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char my_char[] = "_putchar";
+	int i;
+
+	for (i = 0; i < my_char[i]; i++)
+	{
+		putchar(my_char[i]);
+	}
+	putchar('\n');
+	return (0);
+}
