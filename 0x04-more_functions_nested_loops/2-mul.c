b530bc704bf4eacd3bcb4fc9567d7c4c1bb97ba1
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-17 19:29:59 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - Checks if character is upper
+ * @c: The character to check
+ *
+ * Return: Always 1 (Success).
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
