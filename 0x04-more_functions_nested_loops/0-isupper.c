e71887fa83af7cf2844c94c6097a425892eb73e8
Foday Jihared Conteh <jiharedc@gmail.com>
2023-08-17 18:43:52 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Checks if character is upper
+ * @c: The character to check
+ *
+ * Return: Always 1 (Success).
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
