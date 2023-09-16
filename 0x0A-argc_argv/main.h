61c66fef18cdf2d3aebc3089a89243c6c9f4c9b0
Foday Jihared Conteh <jiharedc@gmail.com>
2023-09-16 03:43:19 -0700

    Header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,9 @@
 #define _MAIN_H
 
 int main(int argc __attribute__((unused)), char *argv[]);
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int check_num(char *str);
 
 #endif
