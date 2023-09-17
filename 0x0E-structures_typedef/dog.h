de5dcae5030c7fb0472d33794cce4574e4782ecd
Foday Jihared Conteh <jiharedc@gmail.com>
2023-09-17 03:20:40 -0700

    Dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,25 @@
 #define _DOG_H
 
 /**
- * struct dog - Dog's basic info
+ * struct dog - Dog's info
  * @name: Name
  * @age: Age
  * @owner: Owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /**
  * dog_t - typedef for struct dog
+ * @name: Name
+ * @d: dog
+ * @age: Age
+ * @owner: Owner
  */
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
