6847eb7fd39b137dec463fe3a4cf8fe4cb238ad9
Foday Jihared Conteh <jiharedc@gmail.com>
2023-10-30 17:27:17 -0700

    Duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Returns a pointer to a memory
+ *
+ * @str: Pointer string
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	size_t len = strlen(str);
+
+	char *duplicate = (char *)malloc((len + 1) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (duplicate == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
+
