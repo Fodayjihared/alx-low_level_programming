9fecba649602c61d67a2eb2bdc8dbf47c5b0a03a
Foday Jihared Conteh <jiharedc@gmail.com>
2023-11-06 12:30:04 -0800

    Current file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,22 @@
+#include "2-main.h"
 #include <stdio.h>
 
 /**
- * main - entry point
- * 
+ * printFileName - Prints the file where the code is run
+ *
  * Return: Always 0.
  */
-
-int main() {
-    printf("Compiled from file: %s\n", __FILE__);
-    return 0;
+void printFileName(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
 }
 
+/**
+ * main - main entry of the program
+ * Return: always 0.
+ */
+int main(void)
+{
+	printFileName();
+	return (0);
+}
